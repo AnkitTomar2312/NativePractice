3cf2db327d8b6f6aedec383aadd77af76d3faaed
Ankit Tomar <ankitjdec@gmail.com>
2024-03-03 20:30:58 +0530

    learned about the state

--- App.tsx
+++ App.tsx
@@ -5,14 +5,20 @@
  * @format
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, View, Button} from 'react-native';
 
 function App(): React.JSX.Element {
+  const [data, setDate] = useState(100);
+  const fruit = () => {
+    setDate(600);
+    console.log('clicked');
+  };
   return (
     <View>
+      <Text style={{fontSize: 60}}>{data}</Text>
       <Text style={{fontSize: 50}}>Hello React Native</Text>
-      <Button title="Press Here" />
+      <Button title="Press Here" color={'green'} onPress={fruit} />
     </View>
   );
 }
