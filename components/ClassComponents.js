e513c8d420e92cce3f3535c9f8cfd0a45b5bc378
Ankit Tomar <ankitjdec@gmail.com>
2024-03-05 19:31:16 +0530

    class components 20 finish

--- components/ClassComponents.js
+++ components/ClassComponents.js
@@ -1,12 +1,47 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {Component} from 'react';
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
+import Students from './Student';
 
-const ClassComponents = () => {
-  return (
-    <View>
-      <Text>ClassComponents</Text>
-    </View>
-  );
-};
+class App extends Component {
+  render() {
+    fruit = () => {
+      console.log('Apple');
+    };
+    return (
+      <View>
+        <Text style={styles.heading}>Class Component</Text>
+        <TextInput
+          style={{padding: 20, backgroundColor: 'silver'}}
+          placeholder="Enter Your name"
+        />
+        <Button title="login" onPress={this.fruit} />
+        <Students />
+      </View>
+    );
+  }
+}
 
-export default ClassComponents;
+const styles = StyleSheet.create({
+  textBox: {
+    fontSize: 30,
+    fontFamily: 'Arial',
+    padding: 15,
+    backgroundColor: 'blue',
+    color: 'white',
+    marginTop: 20,
+    textAlign: 'center',
+    borderRadius: 10,
+  },
+  heading: {
+    fontSize: 48,
+    textAlign: 'center',
+    color: 'red',
+  },
+  info: {
+    color: 'purple',
+    fontSize: 28,
+    textAlign: 'center',
+  },
+});
+
+export default App;
