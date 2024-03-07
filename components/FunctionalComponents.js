3ada910f215a3ec92c89aa00d4b18cedd8d0e4d0
Ankit Tomar <ankitjdec@gmail.com>
2024-03-07 21:04:35 +0530

    toggling completed

--- components/FunctionalComponents.js
+++ components/FunctionalComponents.js
@@ -4,22 +4,32 @@ import {create} from 'react-test-renderer';
 
 const FunctionalComponents = () => {
   const [count, setCount] = useState(0);
+  const [show, setShow] = useState(true);
   useEffect(() => {
     console.warn('Count mounted');
   }, [count]);
   return (
     <View>
       <Text style={styles.title}>Functional Components {count}</Text>
+      {show && <User />}
       <Button
         title="Update count"
         onPress={() => {
           setCount(count + 1);
+          setShow(false);
         }}
       />
     </View>
   );
 };
 
+const User = () => {
+  return (
+    <View>
+      <Text style={styles.title}>User Components</Text>
+    </View>
+  );
+};
 const styles = StyleSheet.create({
   title: {
     fontSize: 48,
